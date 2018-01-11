31cf89d8c6bc893ef7ceeb945c89a22cbd3fb444
Karolina <kika.romanska@gmail.com>
2018-01-11 16:23:55 +0100

    Key detection added exercise 12/30 JS

--- script.js
+++ script.js
@@ -166,5 +166,59 @@ console.log(sortDown);
 const sortedByLength = strings.sort((a, b) => a.length > b.length ? 1 : -1);
 console.log(sortedByLength);
 
+//Array cardio day2 ! 
+
+const people2 = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+  ];
+  const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+  ];
+  // Some and Every Checks
+  // Array.prototype.some() // is at least one person 19 or older?
+    const isAdult = people2.some(person => (new Date()).getFullYear()- person.year >= 19);
+    console.log({isAdult});
+
+  // Array.prototype.every() // is everyone 19 or older?
+  const allAdults = people2.every(person => (new Date()).getFullYear()- person.year >= 19);
+  console.log({allAdults});
+  // Array.prototype.find()
+  // Find is like filter, but instead returns just the one you are looking for
+  // find the comment with the ID of 823423
+  const comment = comments.find(comment => comment.id === 823423);
+  console.log(comment);
+
+  // Array.prototype.findIndex()
+  // Find the comment with this ID
+  // delete the comment with the ID of 823423
+
+  const index = comments.findIndex(comment => comment.id === 823423);
+  console.log(index);
+  comments.splice(index, 1);
+
+  // Key Detection 
+
+  const pressed = [];
+  const secretCode = 'karolinka';
+
+  window.addEventListener('keyup', (e) => {
+      pressed.push(e.key)
+      console.log(e.key);
+      console.log(pressed);
+      pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+      if (pressed.join('').includes(secretCode)) {
+          console.log('DING DING !!!');
+          cornify_add();
+      }
+  }); 
+
+
 
 
